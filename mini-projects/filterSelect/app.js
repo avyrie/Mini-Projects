cca80d9f618ee3aa8be9c2f66594f6e06f0d5c74
avyrie <pittman.ally@gmail.com>
2021-03-09 16:18:05 -0700

    flip card and filter projects

---
+++ mini-projects/filterSelect/app.js
@@ -0,0 +1,31 @@
+const addClass = (element, name) => {
+    let arr1 = element.className.split(" ");
+    let arr2 = name.split(" ")
+    for (let i = 0; i < arr2.length; i++) {
+        if (arr1.indexOf(arr2[i]) == -1) {
+            element.className += " " + arr2[i];
+        }
+    }
+}
+
+const removeClass = (element, name) => {
+    let arr1 = element.className.split(" ");
+    let arr2 = name.split(" ");
+    for (let i = 0; i < arr2.length; i++) {
+        while (arr1.indexOf(arr2[i]) > -1) {
+            arr1.splice(arr1.indexOf(arr2[i]), 1)
+        }
+    }
+    element.className = arr1.join(" ")
+}
+
+const filterObjects = (c) => {
+    let x = document.getElementsByClassName("box");
+    if (c == "all") c = "";
+    for (let i = 0; i < x.length; i++) {
+        removeClass(x[i], "show");
+        if (x[i].className.indexOf(c) > -1) addClass(x[i], "show");
+    }
+}
+
+filterObjects('all');
